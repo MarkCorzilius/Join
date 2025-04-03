127e74354d37b1f1f9500f51a6a76d0ba2fa38f7
Mark Corzilius <marccorzprivate@gmail.com>
2025-04-03 12:57:04 +0200

    resolving img and ref visualization on ftp and vs code

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,5 +1,1 @@
-function buttonClicked(element) {
-  const buttons = document.querySelectorAll(".nav-button");
-  buttons.forEach((btn) => btn.classList.remove("clicked-color"));
-  element.classList.add("clicked-color");
-}
+
