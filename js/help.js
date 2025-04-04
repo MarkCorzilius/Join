0d779143b5c4b65e2e307b7df1f36ca726c98d10
victorietta <victorialatza@gmail.com>
2025-04-04 08:08:58 +0100

    update help page: HelpIcon hidden

---
+++ js/help.js
@@ -0,0 +1,7 @@
+function hideHelpIcon() {
+    document.getElementById('helpIcon').classList.add('d-none');
+}
+
+function showHelpIcon() {
+    document.getElementById('helpIcon').classList.remove('d-none');
+}
