15fb679dfe28a834b858fd91edc1c33cb162efaf
Mark Corzilius <marccorzprivate@gmail.com>
2025-06-12 19:47:53 +0200

    fixed VIEWER STATE on privacy and legal pages; working on bubble for contacts in board in task templates

--- js/sidebar.js
+++ js/sidebar.js
@@ -12,3 +12,18 @@
     });
   }
 }
+
+
+function displayLegalNoticeAndPrivacyPolicy() {
+  const footer = document.getElementById('sidebarFooter');
+  const user = JSON.parse(localStorage.getItem('user'))
+  if (user.name === 'Viewer' && window.innerWidth <= 1000) {
+    if (footer) {
+      footer.style.display = 'flex';
+    }
+  } else if (user.name !== "Viewer" || window.innerWidth <= 1000) {
+    if (footer) {
+      footer.style.display = 'none';
+    }
+  }
+}
